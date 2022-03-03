6223fd8ddbd9b1dfe146f3c06a62ca8ad536cd1b
ryan hughes <ryanhuga@gmail.com>
2022-03-03 15:40:48 -0800

    server setup to deploy to heruko

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.static(__dirname + "/dist/<app-name>"));
+app.use(express.static(__dirname + "/dist/chiropractic"));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/chiropractic/index.html"));
 });
 app.listen(process.env.PORT || 8080);
+
+console.log("ran");
